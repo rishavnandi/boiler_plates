1da50eb38436dfdb1b7600a8318a0daf8f825c70
Rishav Nandi <rishavnandi2@gmail.com>
2022-04-09 12:45:10 +0530

    Cleaned Up Docker Files

--- Automated_Deployment/Install_Docker-Compose.sh
+++ Automated_Deployment/Install_Docker-Compose.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
+
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-
-sleep 10
-
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
--- Automated_Deployment/Install_Docker.sh
+++ Automated_Deployment/Install_Docker.sh
@@ -6,18 +6,12 @@ apt-get install \
     gnupg \
     lsb-release
 
-sleep 5
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-sleep 5
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-
-sleep 5
 
 apt-get install docker-ce docker-ce-cli containerd.io -y
