8fabeca8b1fe6f1654618ed7b9c75ef4493a82c0
Rishav Nandi <rishavnandi2@gmail.com>
2022-06-09 17:28:54 +0530

    Updated Docker Compose Version

--- Automated_Deployment/Install_Docker-Compose.sh
+++ Automated_Deployment/Install_Docker-Compose.sh
@@ -2,5 +2,5 @@
 
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
